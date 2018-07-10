9e9d54bd28f4399fe8a9fd3ff6238a522c2dbb00
Totul <rytotul@gmail.com>
2018-07-10 15:51:54 +0600

    Some Changes are made

--- Favourite_Packages_Installer.py
+++ Favourite_Packages_Installer.py
@@ -124,6 +124,7 @@ class ZSH():
         #   Installs and customize zsh shell
         _current_directory = getcwd()
         # Downloads and Copies oh-my-zsh plugin
+        remove('~/.oh-my-zsh')
         system('git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh')
         # Copy the Configuration file to Home Directory
         system('sudo cp ' + _current_directory + '/.zshrc ~/')
