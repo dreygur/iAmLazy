9d13b45fba8711bf12e2df7cc6406a9d592d3379
Rakibul Yeasin <rytotul@gmail.com>
2019-05-23 04:02:23 +0600

    Update codebloks-installer.sh

--- Bash/codebloks-installer.sh
+++ Bash/codebloks-installer.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 
-:"
+:'
     Code::BLocks Installer for Debian/Ubuntu/Linux-Mint
     Author: Rakibul Yeasin
-"
+'
 
 install() {
     if [[ -z $( dpkg -l | grep "codeblocks" ) ]]; then
         echo -e "Code::BLocks IDE is not installed...\nInstalling...\n"
         echo -e "Adding PPA..."
-        $( sudo add-apt-repository ppa:damien-moore/codeblocks-stable -y )
+        sudo add-apt-repository ppa:damien-moore/codeblocks-stable -y
         echo -e "PPA Added...\nUpdating Cache..."
-        $( sudo apt-get update)
+        sudo apt-get update
         echo -e "Fetching Pachage from server..."
-        $( sudo apt-get install codeblocks codeblocks-contrib -y )
+        sudo apt-get install codeblocks codeblocks-contrib -y
 
         if [[ -n $( dpkg -l | grep "codeblocks" ) ]]; then
             echo -e "Succesfully installed Code::Blocks IDE..."
