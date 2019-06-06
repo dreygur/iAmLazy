fda9508993d2111facbb87ad2397d5f5cbb6fe42
Rakibul Yeasin <rytotul@gmail.com>
2019-06-06 21:00:36 +0600

    Updated

--- Python/Install Themes.sh
+++ Python/Install Themes.sh
@@ -12,8 +12,37 @@
 #
 # 05:06:2019 02:41:AM WEDNESDAY
 
+distro () {
+    # Distro detector
+    # Detects Currently Installed Distribution
 
-function themes() {
+    local DISTRO_NAME DISTRO_DATA DISTRO_STR
+    # Checking Current Distro
+    for line in `cat /etc/*-release`; do
+        if [[ $line =~ ^(NAME|DISTRIB_ID)=(.+)$ ]]; then
+            DISTRO_STR=${BASH_REMATCH[2]}
+            if echo $DISTRO_STR | grep -q ['ubuntu','debian','mint']; then
+                DISTRO_NAME='debian'
+            elif echo $DISTRO_STR | grep -q ['fedora']; then
+                DISTRO_NAME='fedora'
+            elif echo $DISTRO_STR | grep -q ['arch','manjaro','antergos','parabola','anarchy']; then
+                DISTRO_NAME='arch'
+            fi
+            break
+        fi
+    done
+    # plain arch installation doesn't guarantee /etc/os-release
+    # but the filesystem pkg installs a blank /etc/arch-release
+    if [ -z "$DISTRO_NAME" ]; then
+        if [ -e /etc/arch-release ]; then
+        DISTRO_NAME='arch'
+        fi
+    fi
+    # TODO: if os is not detected, try package-manager detection (not 100% reliable)
+    #echo $DISTRO_NAME
+}
+
+themes () {
     # Theme installer
     # Need to Implement downloading feature
     # But, the download link is temporary :(
@@ -23,33 +52,46 @@ function themes() {
 
     # Download Icon-Pack
     # wget -O Flat-Remix.tar.xz
-    cp ./Python/assets/Flat-Remix.tar.xz ~/.icons/ && tar -xf ~/.icons/Flat-Remix.tar.xz -C ~/.icons/
+    cp ./Python/assets/Flat-Remix.tar.xz $HOME/.icons/
+    tar -xf $HOME/.icons/Flat-Remix.tar.xz -C $HOME/.icons/
 
     # Download xfce4 theme
     # wget -O McOS.tar.gz
-    cp ./Python/assets/McOS.tar.gz ~/.themes/ && tar -xf ~/.themes/McOS.tar.gz -C ~/.themes/
+    cp ./Python/assets/McOS.tar.gz $HOME/.themes/
+    tar -xf $HOME/.themes/McOS.tar.gz -C $HOME/.themes/
     # Clean the Directory
-    rm ~/.themes/McOS.tar.gz ~/.icons/Flat-Remix.tar.xz
+    rm $HOME/.themes/McOS.tar.gz $HOME/.icons/Flat-Remix.tar.xz
 }
 
-function config() {
-    # Configuring
-
+install_plank () {
     # Install Plank
-    sudo add-apt-repository ppa:ricotz/docky -y
-    sudo apt-get update -y && sudo apt-get install -y plank
 
+    echo "Installing Plank"
+    if [ `distro` == 'debian' ]; then
+        sudo add-apt-repository ppa:ricotz/docky -y
+        sudo apt-get update -y
+        sudo apt-get install -y plank
+    elif [ `distro` == 'arch' ]; then
+        sudo pacman -Syu plank --noconfirm
+    fi
+
+    echo "Preparing Plank to autostart..."
     echo "[Desktop Entry]
-    Encoding = UTF-8
-    Version = 0.9.4
-    Type = Application
-    Name = Plank
-    Comment = Plank Dock
-    Exec = /usr/bin/plank
-    OnlyShowIn = XFCE;
-    StartupNotify = false
-    Terminal = false
-    Hidden = false" | sudo tee -a $HOME/.config/autostart/PLank.desktop
+    Encoding=UTF-8
+    Version=0.11.4
+    Type=Application
+    Name=Plank
+    Comment=Plank Dock
+    Exec=/usr/bin/plank
+    OnlyShowIn=XFCE;
+    StartupNotify=false
+    Terminal=false
+    Hidden=false" | sudo tee -a $HOME/.config/autostart/Plank.desktop
+}
+
+config () {
+    # Installing Plank
+    install_plank
 
     # Configure Theme
     xfconf-query -c xsettings -p /Net/ThemeName -s "McOS-MJV-Dark-XFCE-Edition-2.3"
