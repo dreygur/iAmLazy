f3315b35e371f0f8f9eb05480cea1e196a7a063d
Rakibul Yeasin <rytotul@gmail.com>
2019-06-19 18:12:09 +0600

    Update popcorntime.sh

--- Bash/popcorntime.sh
+++ Bash/popcorntime.sh
@@ -10,7 +10,7 @@ echo "Configuring..."
 sudo apt install -y libcanberra-gtk-module libgconf-2-4
 
 echo "Downloading the Package..."
-wget "https://get.popcorntime.sh/build/Popcorn-Time-0.3.10-Linux-64.tar.xz" -O popcorntime.tar.xz
+wget -O popcorntime.tar.xz "https://get.popcorntime.sh/build/Popcorn-Time-0.3.10-Linux-64.tar.xz"
 
 sudo mkdir /opt/popcorntime
 sudo tar -xf popcorntime.tar.xz -C /opt/popcorntime
@@ -19,14 +19,16 @@ sudo ln -sf /opt/popcorntime/Popcorn-Time /usr/bin/Popcorn-Time
 
 echo "Setting up Desktop Entry..."
 echo "[Desktop Entry]
-Version = 1.0
-Type = Application
-Terminal = false
-Name = Popcorn Time
-Exec = /usr/bin/Popcorn-Time
-Icon = /opt/popcorntime/popcorn.png
-Categories = AudioVideo;Player;Recorder;" | sudo tee -a /usr/share/applications/popcorntime.desktop
+Version=1.0
+Type=Application
+Terminal=false
+Name=Popcorn Time
+Exec=/usr/bin/Popcorn-Time
+Icon=/opt/popcorntime/popcorn.png
+Categories=AudioVideo;Player;Recorder;" | sudo tee -a /usr/share/applications/popcorntime.desktop
 
 sudo wget -O /opt/popcorntime/popcorn.png https://upload.wikimedia.org/wikipedia/commons/d/df/Pctlogo.png
+
+rm Popcorn-Time-0.3.10-Linux-64.tar.xz
 
 echo "Done!!!"
