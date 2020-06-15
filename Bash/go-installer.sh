d6e27efd8a91e1b1d8e32806a1333e09c82ea1e4
Rakibul Yeasin <rytotul@gmail.com>
2020-06-15 13:45:47 +0600

    Added GoLang installer :fire:

--- Bash/go-installer.sh
+++ Bash/go-installer.sh
@@ -6,82 +6,66 @@ VERSION="1.14"
 [ -z "$GOROOT" ] && GOROOT="$HOME/.go"
 [ -z "$GOPATH" ] && GOPATH="$HOME/go"
 
-OS="$(uname -s)"
 ARCH="$(uname -m)"
-
-case $OS in
-    "Linux")
-        case $ARCH in
-        "x86_64")
-            ARCH=amd64
-            ;;
-        "aarch64")
-            ARCH=arm64
-            ;;
-        "armv6")
-            ARCH=armv6l
-            ;;
-        "armv8")
-            ARCH=arm64
-            ;;
-        .*386.*)
-            ARCH=386
-            ;;
-        esac
-        PLATFORM="linux-$ARCH"
-    ;;
-    "Darwin")
-        PLATFORM="darwin-amd64"
-    ;;
-esac
+	case $ARCH in
+	"x86_64")
+		ARCH=amd64
+		;;
+	"aarch64")
+		ARCH=arm64
+		;;
+	"armv6")
+		ARCH=armv6l
+		;;
+	"armv8")
+		ARCH=arm64
+		;;
+	.*386.*)
+		ARCH=386
+		;;
+	esac
+	PLATFORM="linux-$ARCH"
+ 
 
 print_help() {
-    echo "Usage: bash goinstall.sh OPTIONS"
-    echo -e "\nOPTIONS:"
-    echo -e "  --remove\tRemove currently installed version"
-    echo -e "  --version\tSpecify a version number to install"
+	echo "Usage: bash goinstall.sh OPTIONS"
+	echo -e "\nOPTIONS:"
+	echo -e "  --remove\tRemove currently installed version"
+	echo -e "  --version\tSpecify a version number to install"
 }
 
 if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-    shell_profile="zshrc"
+	shell_profile="zshrc"
 elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-    shell_profile="bashrc"
+	shell_profile="bashrc"
 fi
 
 if [ "$1" == "--remove" ]; then
-    rm -rf "$GOROOT"
-    if [ "$OS" == "Darwin" ]; then
-        sed -i "" '/# GoLang/d' "$HOME/.${shell_profile}"
-        sed -i "" '/export GOROOT/d' "$HOME/.${shell_profile}"
-        sed -i "" '/$GOROOT\/bin/d' "$HOME/.${shell_profile}"
-        sed -i "" '/export GOPATH/d' "$HOME/.${shell_profile}"
-        sed -i "" '/$GOPATH\/bin/d' "$HOME/.${shell_profile}"
-    else
-        sed -i '/# GoLang/d' "$HOME/.${shell_profile}"
-        sed -i '/export GOROOT/d' "$HOME/.${shell_profile}"
-        sed -i '/$GOROOT\/bin/d' "$HOME/.${shell_profile}"
-        sed -i '/export GOPATH/d' "$HOME/.${shell_profile}"
-        sed -i '/$GOPATH\/bin/d' "$HOME/.${shell_profile}"
-    fi
-    echo "Go removed."
-    exit 0
+	rm -rf "$GOROOT"
+	sed -i '/# GoLang/d' "$HOME/.${shell_profile}"
+	sed -i '/export GOROOT/d' "$HOME/.${shell_profile}"
+	sed -i '/$GOROOT\/bin/d' "$HOME/.${shell_profile}"
+	sed -i '/export GOPATH/d' "$HOME/.${shell_profile}"
+	sed -i '/$GOPATH\/bin/d' "$HOME/.${shell_profile}"
+	echo "Go removed."
+	exit 0
 elif [ "$1" == "--help" ]; then
-    print_help
-    exit 0
+	print_help
+	exit 0
 elif [ "$1" == "--version" ]; then
-    if [ -z "$2" ]; then # Check if --version has a second positional parameter
-        echo "Please provide a version number for: $1"
-    else
-        VERSION=$2
-    fi
+	if [ -z "$2" ]; then # Check if --version has a second positional parameter
+		echo "Please provide a version number for: $1"
+	else
+		VERSION=$2
+	fi
 elif [ ! -z "$1" ]; then
-    echo "Unrecognized option: $1"
-    exit 1
+	echo "Unrecognized option: $1"
+	exit 1
 fi
 
 if [ -d "$GOROOT" ]; then
-    echo "The Go install directory ($GOROOT) already exists. Exiting."
-    exit 1
+	echo "The Go install directory ($GOROOT) already exists. Exiting."
+	exit 1
 fi
 
 PACKAGE_NAME="go$VERSION.$PLATFORM.tar.gz"
@@ -89,14 +73,14 @@ TEMP_DIRECTORY=$(mktemp -d)
 
 echo "Downloading $PACKAGE_NAME ..."
 if hash wget 2>/dev/null; then
-    wget https://storage.googleapis.com/golang/$PACKAGE_NAME -O "$TEMP_DIRECTORY/go.tar.gz"
+	wget https://storage.googleapis.com/golang/$PACKAGE_NAME -O "$TEMP_DIRECTORY/go.tar.gz"
 else
-    curl -o "$TEMP_DIRECTORY/go.tar.gz" https://storage.googleapis.com/golang/$PACKAGE_NAME
+	curl -o "$TEMP_DIRECTORY/go.tar.gz" https://storage.googleapis.com/golang/$PACKAGE_NAME
 fi
 
 if [ $? -ne 0 ]; then
-    echo "Download failed! Exiting."
-    exit 1
+	echo "Download failed! Exiting."
+	exit 1
 fi
 
 echo "Extracting File..."
@@ -104,11 +88,11 @@ mkdir -p "$GOROOT"
 tar -C "$GOROOT" --strip-components=1 -xzf "$TEMP_DIRECTORY/go.tar.gz"
 touch "$HOME/.${shell_profile}"
 {
-    echo '# GoLang'
-    echo "export GOROOT=${GOROOT}"
-    echo 'export PATH=$GOROOT/bin:$PATH'
-    echo "export GOPATH=$GOPATH"
-    echo 'export PATH=$GOPATH/bin:$PATH'
+	echo '# GoLang'
+	echo "export GOROOT=${GOROOT}"
+	echo 'export PATH=$GOROOT/bin:$PATH'
+	echo "export GOPATH=$GOPATH"
+	echo 'export PATH=$GOPATH/bin:$PATH'
 } >> "$HOME/.${shell_profile}"
 
 mkdir -p $GOPATH/{src,pkg,bin}
