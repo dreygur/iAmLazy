2c5180254985eb6973ed269fe41b33aff0628820
Rakibul Yeasin <rytotul@gmail.com>
2020-09-20 20:21:13 +0600

    :mag: A bug Found

--- Golang/github.go
+++ Golang/github.go
@@ -13,13 +13,14 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/imroc/req"
 )
 
 func create(repoName string, private bool) {
-	username := "" // Github Username
-	token := ""    // Github Personal Auth-Token
+	username := "dreygur"                               // Github Username
+	token := "1a1606d93e1b05099e893145e2199cfa098baa38" // Github Personal Auth-Token
 
 	// HTTP-Auth (base64)
 	userToken := username + ":" + token
@@ -36,7 +37,7 @@ func create(repoName string, private bool) {
 
 	// Is it a Private Repo or Not
 	prv := "false"
-	if private {
+	if private == true {
 		prv = "true"
 	}
 
@@ -73,15 +74,37 @@ func create(repoName string, private bool) {
 
 func main() {
 	// Gets the repo-name
-	repoName := flag.String("n", "", "Name of your desired REPO!")
-	private := flag.Bool("p", false, "Is the repo private or not?")
+	private := flag.Bool("p", true, "")
+
 	// Show Usage Info
 	flag.Usage = func() {
-		fmt.Printf("Usage: Github Auto repo Creator [OPTIONS] argument ...\n")
-		flag.PrintDefaults()
+		h := "Usage: Github Auto repo Creator [OPTIONS] argument ...\n\n"
+
+		h += "Usage:\n"
+		h += "  github [NAME] [OPTIONS]\n\n"
+
+		h += "Options:\n"
+		h += "  -p, --ungron     Is the repo private or not?\n"
+		h += "      --version    Print version information\n\n"
+
+		h += "Examples:\n"
+		h += "  github Test\n"
+		h += "  github Test -p\n"
+
+		fmt.Fprintf(os.Stderr, h)
 	}
 	// Parse Commands
 	flag.Parse()
-	// Creating Repo on github and a directory with that on local
-	create(*repoName, *private)
+
+	// RepoName
+	repoName := flag.Arg(0)
+	if repoName == "" {
+		fmt.Println("[*] Please Provide a REPO name")
+	} else if strings.HasPrefix(repoName, "-") {
+		flag.Usage()
+	} else {
+		// Creating Repo on github and a directory with that on local
+		fmt.Println(*private)
+		// create(repoName, *private)
+	}
 }
