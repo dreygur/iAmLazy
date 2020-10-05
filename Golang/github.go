a4822e8c42fa13e6ae356ad0ce808d1fbb857381
Rakibul Yeasin <rytotul@gmail.com>
2020-10-06 02:44:46 +0600

    :fire: Fixed the bug! :fire:

--- Golang/github.go
+++ Golang/github.go
@@ -13,13 +13,44 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 
 	"github.com/imroc/req"
 )
 
+// Body Structure as json
+type Body struct {
+	Name    string `json:"name"`
+	Private *bool  `json:"private,omitempty"`
+}
+
+var reset string = "\033[0m"
+var red string = "\033[31m"
+var green string = "\033[32m"
+var yellow string = "\033[33m"
+var blue string = "\033[34m"
+var purple string = "\033[35m"
+var cyan string = "\033[36m"
+var gray string = "\033[37m"
+var white string = "\033[97m"
+
+func init() {
+	if runtime.GOOS == "windows" {
+		reset = ""
+		red = ""
+		green = ""
+		yellow = ""
+		blue = ""
+		purple = ""
+		cyan = ""
+		gray = ""
+		white = ""
+	}
+}
+
 func create(repoName string, private bool) {
-	username := "" // Github Username
-	token := ""    // Github Personal Auth-Token
+	username := ""                               // Github Username
+	token := "" // Github Personal Auth-Token
 
 	// HTTP-Auth (base64)
 	userToken := username + ":" + token
@@ -35,16 +66,14 @@ func create(repoName string, private bool) {
 	}
 
 	// Is it a Private Repo or Not
-	prv := "false"
+	prv := new(bool)
+	*prv = false
 	if private == true {
-		prv = "true"
+		*prv = true
 	}
 
 	// Basic commands to Create the repo as body JSON
-	body, _ := json.Marshal(map[string]string{
-		"name":    repoName,
-		"private": prv,
-	})
+	body, _ := json.Marshal(Body{repoName, prv})
 
 	// Getting just the JSON response
 	req.SetFlags(req.LrespBody)
@@ -54,8 +83,9 @@ func create(repoName string, private bool) {
 	}
 	var dest map[string]interface{}
 	res.ToJSON(&dest)
+
 	if dest["ssh_url"] != nil {
-		fmt.Printf("[+] SSH URL: %+v\n", dest["ssh_url"])
+		fmt.Printf("%v[+] SSH URL: %+v%v\n", blue, dest["ssh_url"], reset)
 		_, err = os.Stat(repoName)
 		if err != nil {
 			// Creating Directory with Write-Execution Mode and changing path to that
@@ -63,16 +93,17 @@ func create(repoName string, private bool) {
 			os.Chdir(repoName)
 			// Initializing Empty git repo
 			out, _ := exec.Command("git", "init").Output()
-			fmt.Printf("[+] %v\n", string(out))
+			fmt.Printf("%v[+] %v%v", green, string(out), reset)
 			// Adding Remote
 			out, _ = exec.Command("git", "remote", "add", "origin", fmt.Sprintf("%v", dest["ssh_url"])).Output()
-			fmt.Printf("[+] Success\n")
+			fmt.Printf(green + "[+] Success!\n" + reset)
+		} else {
+			fmt.Println(red + "[-] Directory Already Exists!" + reset)
 		}
 	}
 }
 
 func main() {
-	// Gets the repo-name
 	private := flag.Bool("p", false, "")
 	// Parse Commands
 	flag.Parse()
@@ -97,10 +128,10 @@ func main() {
 	if len(os.Args) > 2 {
 		raw := os.Args[2]
 		// Creating Repo on github and a directory with that on local
-		create(repoName, *private)
+		create(raw, *private)
 	} else if len(os.Args) == 2 {
 		raw := os.Args[1]
-		create(repoName, *private)
+		create(raw, *private)
 	} else {
 		flag.Usage()
 	}
