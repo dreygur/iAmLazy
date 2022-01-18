30de7fc129eb1ac2a739ea6e9eb89ba0a2c8c44b
Rakibul Yeasin <ryeasin03@gmail.com>
2022-01-19 01:40:51 +0600

    Documented :fire:

--- Google Scripts/openseaCollectionFloorPrice.js
+++ Google Scripts/openseaCollectionFloorPrice.js
@@ -1,14 +1,67 @@
-function getFloorPrice(collection) {
-  var option = {
-    headers : {
-      "Accept": "application/json"
-    }
-  };
+/**************************************************
+ * 
+ * Opesea.io
+ * This script parses `floor_parse` from opensea
+ * using the api (as free and anonymous user)
+ * with collection `slug` or URL
+ * and saves to the CELL from it was called.
+ * 
+ * This script exposes a custom function named
+ * `getFloorPrice` that processes the data.
+ * 
+ * Author: Rakibul Yeasin <ryeasin03@gmail.com>
+ * 
+ *************************************************/
 
-  var url = `https://api.opensea.io/api/v1/collection/${collection}/stats`;
-  var response = UrlFetchApp.fetch(url, option).getContentText();
+// Request Header to tell opensea to response as JSON
+const option = {
+  headers : {
+    "Accept": "application/json"
+  }
+};
+
+/*
+ * Requests to opensea api and parses the floor price of a `Collection` 
+ * @params {String} collection
+ * @return {String} floorPrice
+ */
+function getFloorPrice(collection) {
+  // Throw ERROR if collection is empty
+  if (collection === '') {
+    return "Please Specify a collection slug/url";
+  }
+
+  const urlChecker= /http?s:\/\//gm
+  let isUrl = urlChecker.test(collection);
+  if (isUrl) {
+    /*
+     * As we only need the collection `slug`
+     * So just splitted the `slug` from url
+     */
+    collection = parseCollection(collection);
+  }
+
+  // API endpoint to get asset stats
+  let url = `https://api.opensea.io/api/v1/collection/${collection}/stats`;
+  let response = UrlFetchApp.fetch(url, option).getContentText();
   response = JSON.parse(response);
 
-  Logger.log(response?.stats?.floor_price);
-  return response?.stats?.floor_price;
+  // The Floor Price
+  let floorPrice = response?.stats?.floor_price;
+  return floorPrice
 }
+
+/*
+ * Parses opesea url to collection `slug`
+ * @params {String} url
+ * @return {String} slug
+ */
+function parseCollection(url) {
+  let theUrlArray = url.split('/');
+  // Final Slug
+  let slug = theUrlArray[theUrlArray.length - 1];
+  return slug
+}
+
+// Testing Log
+Logger.log(getFloorPrice('https://opensea.io/collection/wolf-game-farmer'))
